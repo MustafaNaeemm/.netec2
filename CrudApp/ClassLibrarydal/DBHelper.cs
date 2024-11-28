ff4cb49a836dd07086e1793d3b0ee6af6bd31f4a
MustafaNaeemm <mustafanaeemahmadkhan@gmail.com>
2024-11-28 18:27:45 +0500

    first commit .net

---
+++ CrudApp/ClassLibrarydal/DBHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace ClassLibrarydal
+{
+    public class DBHelper
+    {
+        public static SqlConnection GetConnection()
+        {
+            SqlConnection con = new SqlConnection("Data Source=MUSTAFA\\MSSQLSERVER01;Initial Catalog=Students;Integrated Security=True");
+            return con;
+        }
+    }
+}
---
+++ CrudApp/ClassLibrarydal/DalStudent.cs
@@ -0,0 +1,80 @@
+﻿using System.Data;
+using ModelClass;
+using System;
+using System.Collections.Generic;
+
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace ClassLibrarydal
+{
+    public class DalStudent
+    {
+        public static void SaveStudentInformation(ModelStudent ms)
+        {
+            SqlConnection conn = DBHelper.GetConnection();
+            conn.Open();
+            SqlCommand cmd = new SqlCommand("SP_SaveStudent", conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@FirstName", ms.FirstName);
+            cmd.Parameters.AddWithValue("@EmailAddress", ms.EmailAddress);
+            cmd.ExecuteNonQuery();
+            conn.Close();
+        }
+
+        public static void DeleteStudentInformation(int id)
+        {
+            SqlConnection con = DBHelper.GetConnection();
+            con.Open();
+            SqlCommand cmd = new SqlCommand("SP_DeleteStudents", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@StuId", id);
+            cmd.ExecuteNonQuery();
+            con.Close();
+        }
+        public static void UpdateStudentInformation(ModelStudent ms)
+        {
+            SqlConnection con = DBHelper.GetConnection();
+            con.Open();
+            SqlCommand cmd = new SqlCommand("SP_UpdateStudents", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@StuId", ms.StuId);
+            cmd.Parameters.AddWithValue("@FirstName", ms.FirstName);
+            cmd.Parameters.AddWithValue("@EmailAddress", ms.EmailAddress);
+            cmd.ExecuteNonQuery();
+            con.Close();
+        }
+
+        public static List<ModelStudent> GetStudentInformation()
+        {
+            SqlConnection con = DBHelper.GetConnection();
+            con.Open();
+            SqlCommand cmd = new SqlCommand("SP_GetStudents", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            SqlDataReader reader = cmd.ExecuteReader();
+            List<ModelStudent> listStudent = new List<ModelStudent>();
+            while (reader.Read())
+            {
+                ModelStudent student = new ModelStudent();
+                student.StuId = Convert.ToInt32(reader["StuId"]);
+                student.FirstName = reader["FirstName"].ToString();
+                student.EmailAddress = reader["EmailAddress"].ToString();
+                listStudent.Add(student);
+
+            }
+            con.Close();
+            return listStudent;
+
+
+        }
+
+
+    }
+
+}
+
+
